cf6ae7ed19538d57273761b71040ad1a16a1d2ff
7e1ght <7e1ght.jm@gmail.com>
2020-03-22 21:00:17 +0200

    ListContainer

---
+++ include/containers/ListContainer.hpp
@@ -0,0 +1,15 @@
+#ifndef LIST_CONTAINER_HPP
+#define LIST_CONTAINER_HPP
+
+#include "support/support.hpp"
+#include "containers/IContainerBase.hpp"
+#include "support/Vector.hpp"
+
+class ListContainer
+    : public IContainerBase
+{
+private:
+    
+};
+
+#endif // LIST_CONTAINER_HPP
