41f92181c3b9523ab8c62185b9970b47e2d7fd82
7e1ght <7e1ght.jm@gmail.com>
2020-03-25 21:40:10 +0200

    add ListContainer.hpp

--- include/containers/ListContainer.hpp
+++ include/containers/ListContainer.hpp
@@ -3,13 +3,30 @@
 
 #include "support/support.hpp"
 #include "containers/IContainerBase.hpp"
+#include "containers/TripleContainer.hpp"
+#include "EmptyContainer.hpp"
 #include "support/Vector.hpp"
 
 class ListContainer
     : public IContainerBase
 {
-private:
-    
+    private:
+        using Item = TripleContainer;
+
+        EmptyContainer mMainContainer;
+
+        supp::Vector<Item*> mDrawItems;
+        supp::Vector<Item*> mItemBuffer;
+
+        void changeInsidePositionSize(Item* item);
+
+        const uint8_t mDrawItemCount;
+        const uint8_t mItemHeight;
+    public:
+        void addItem(Item* newItem);
+        void addItem(IContainerBase* leftBlock, IContainerBase* middleBlock, IContainerBase* rightBlock);
+
+        ListContainer(const uint8_t drawItemCount, const supp::Point& position, const supp::Size& size, const supp::Color& color, IContainerBase* parent);
 };
 
 #endif // LIST_CONTAINER_HPP
